238a9e6227cb6f78222d441ae4d07160a15c6bdf
Dionysus <zetatez@icloud.com>
2023-04-01 00:49:16 +0800

    cmt

--- config.def.h
+++ config.def.h
@@ -94,10 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 4;
 
 /* bg opacity */
-float alpha = 0.62, alphaUnfocused = 0.32;
-/* float alpha = 0.72, alphaUnfocused = 0.64; */
-/* float alpha = 0.81, alphaUnfocused = 0.72; */
-/* float alpha = 0.84, alphaUnfocused = 0.72; */
+float alpha = 0.81, alphaUnfocused = 0.48;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
