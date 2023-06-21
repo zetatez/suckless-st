3eb8a0dc10b187c19b7e717892911e0f885efc91
Dionysus <zetatez@icloud.com>
2023-06-21 20:11:58 +0800

    cmt

--- config.def.h
+++ config.def.h
@@ -95,9 +95,9 @@ char *termname = "st-256color";
 unsigned int tabspaces = 4;
 
 /* bg opacity */
-// float alpha = 0.81, alphaUnfocused = 0.64;
-float alpha = 0.24, alphaUnfocused = 0.32;
-
+// float alpha = 0.96, alphaUnfocused = 0.81;
+float alpha = 0.81, alphaUnfocused = 0.64;
+// float alpha = 0.08, alphaUnfocused = 0.16;
 
 static const char *colorname[] = {
 	/* 0-7 8 normal colors */
@@ -107,7 +107,7 @@ static const char *colorname[] = {
 	"yellow3",
 	"blue2",
 	"magenta3",
-	"#164349",
+	"#005577",
 	"gray90",
 
 	/* 8-15 8 bright colors */
@@ -133,16 +133,16 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 6; // foreground color: font color
+unsigned int defaultfg = 0; // foreground color: font color
 unsigned int defaultbg = 6;
-unsigned int defaultcs = 2;  // cursor color
+unsigned int defaultcs = 5;  // cursor color
 static unsigned int defaultrcs = 0;
 unsigned int bg = 6, bgUnfocused = 6; // focus st color and unfocus st color                                                                                                    // st-focus
-unsigned int const currentBg = 5, buffSize = 2048; // normal mode: alt-n cross + color                                                                                          // st -meta-vim-full
+unsigned int const currentBg = 6, buffSize = 2048; // normal mode: alt-n cross + color                                                                                          // st -meta-vim-full
 /// Enable double / triple click yanking / selection of word / line.                                                                                                            // st -meta-vim-full
 int const mouseYank = 1, mouseSelect = 1;                                                                                                                                       // st -meta-vim-full
 /// [Vim Browse] Colors for search results currently on screen.                                                                                                                 // st -meta-vim-full
-unsigned int const highlightBg = 3, highlightFg = 6;                                                                                                                            // st -meta-vim-full
+unsigned int const highlightBg = 2, highlightFg = 3;                                                                                                                            // st -meta-vim-full
 char const wDelS[] = "!\"#$%&'()*+,-./:;<=>?@[\\]^`{|}~", wDelL[] = " \t";                                                                                                      // st -meta-vim-full
  //< Shortcusts executed in normal mode                                                                                                                                         // st -meta-vim-full
 char *nmKeys [] = { "R/Building\nN", "r/Building\n", "X/juli@machine\nN", "x/juli@machine\n", "Q?[Leaving vim, starting execution]\n","F/: error:\nN", "f/: error:\n", "DQf" }; // st -meta-vim-full
