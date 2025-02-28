03e1adefd828c6437c20194a6a1854cde6d8a183
ShiYi <zetatez@icloud.com>
2025-02-28 23:20:04 +0800

    cmt

--- config.def.h
+++ config.def.h
@@ -95,7 +95,7 @@ unsigned int tabspaces = 4;
 
 /* bg opacity */
 // float alpha = 1, alphaUnfocused = 1;
-float alpha = 1.00, alphaUnfocused = 0.92;
+float alpha = 0.94, alphaUnfocused = 0.82;
 // float alpha = 0.90, alphaUnfocused = 0.76;
 // float alpha = 0.88, alphaUnfocused = 0.76;
 // float alpha = 0.16, alphaUnfocused = 0.08;
