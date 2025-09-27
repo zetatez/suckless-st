e56eb38a228fddf69a6dcc6bda1d225818fe0bf7
ShiYi <zetatez@icloud.com>
2025-09-27 18:30:15 +0800

    cmt

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 // static char *font = "Liberation Mono:pixelsize=20:antialias=true:autohint=true";
-static char *font = "Liberation Mono:pixelsize=24:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=26:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
@@ -97,7 +97,7 @@ unsigned int tabspaces = 4;
 /* bg opacity */
 // float alpha = 1, alphaUnfocused = 1;
 
-float alpha = 0.84, alphaUnfocused = 0.618;
+float alpha = 0.82, alphaUnfocused = 0.618;
 // float alpha = 0.16, alphaUnfocused = 0.08;
 
 static const char *colorname[] = {
