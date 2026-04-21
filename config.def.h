70a23abb3eca55902eeaacc2224b6a5fc399cb77
shiyi <zetatez@icloud.com>
2026-04-21 19:28:53 +0800

    cmt

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=24:antialias=true:autohint=true";
-// static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=38:antialias=true:autohint=true";
+// static char *font = "Liberation Mono:pixelsize=24:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
